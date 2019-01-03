5a906e61d08196d117d05280aa4ca3feaa4595e1
Nicolai Borden <c14nbn@cs.umu.se>
2019-01-03 20:13:26 +0100

    EncoderCore WiP, can now create encoder instances. Working on downloading & converting files from KG's S3 bucket.

--- src/main/java/encoder/EncoderCore.java
+++ src/main/java/encoder/EncoderCore.java
@@ -1,36 +1,56 @@
 package encoder;
 
+import aws.CredentialsFetch;
 import client.Message;
 import client.prototypes.QueueChannelWrapper;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.*;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.transfer.Download;
+import com.amazonaws.services.s3.transfer.Transfer;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferProgress;
 import com.google.gson.Gson;
 import com.rabbitmq.client.*;
+import infrastructure.instances.manager.ManagerInstance;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
+import java.util.Base64;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 public class EncoderCore {
 
-    private BasicAWSCredentials awsCredentials = new BasicAWSCredentials(/*put pub key here*/null,null/*put private key here*/);
-    private AmazonS3Client s3Client = new AmazonS3Client(awsCredentials);
+    private Channel channel;
+    private Connection connection;
 
-   Channel channel;
-   Connection connection;
+    private AmazonS3Client amazonS3Client;
+    private AmazonEC2Client amazonEC2Client;
 
     public static String ENCODING_REQUEST_QUEUE = "encoding-request-queue";
     private String bucket_name = "kgencoderbucket";
 
     public EncoderCore() {
 
+        amazonS3Client = new AmazonS3Client(getAwsCredentials());
+        amazonEC2Client = new AmazonEC2Client(getAwsCredentials());
 
+    }
+
+
+    public BasicAWSCredentials getAwsCredentials() {
+        return new BasicAWSCredentials(CredentialsFetch.getCredentialsProvider().getCredentials().getAWSAccessKeyId(),
+                CredentialsFetch.getCredentialsProvider().getCredentials().getAWSSecretKey());
     }
 
     public File getFile() throws URISyntaxException {
@@ -40,31 +60,78 @@ public class EncoderCore {
     }
 
 
-    public List<S3ObjectSummary> getFilesFromS3() {
+    public List<S3ObjectSummary> listFilesS3() {
 
-        ListObjectsV2Result result = s3Client.listObjectsV2(bucket_name);
+        ListObjectsV2Result result = amazonS3Client.listObjectsV2(bucket_name);
         return result.getObjectSummaries();
     }
 
-    public void getFileFromS3(String key_name) {
 
-        S3Object o = s3Client.getObject(bucket_name, key_name);
-        S3ObjectInputStream s3is = o.getObjectContent();
+    public void getFileFromS3(String key) throws IOException {
 
-        //This is where the downloaded file will be saved
-        File localFile = new File("/home/c14/c14nbn/ddd.mp4");
+        S3Object fullObject = null, objectPortion = null, headerOverrideObject = null;
+        try {
 
-        //This returns an ObjectMetadata file but you don't have to use this if you don't want
-        ObjectMetadata object = s3Client.getObject(new GetObjectRequest(bucket_name, "converted/sample_video.mp4"), localFile);
+            // Get an object and print its contents.
+            System.out.println("Downloading an object");
+            fullObject = amazonS3Client.getObject(new GetObjectRequest(bucket_name, key));
+            System.out.println("Content-Type: " + fullObject.getObjectMetadata().getContentType());
+            System.out.println("Content: ");
+            displayTextInputStream(fullObject.getObjectContent());
 
-        if (localFile.exists() && localFile.canRead()) {
-            System.out.println("IT WORK!!!");
+            // Get a range of bytes from an object and print the bytes.
+            GetObjectRequest rangeObjectRequest = new GetObjectRequest(bucket_name, key)
+                    .withRange(0,9);
+            objectPortion = amazonS3Client.getObject(rangeObjectRequest);
+            System.out.println("Printing bytes retrieved.");
+            displayTextInputStream(objectPortion.getObjectContent());
+
+            // Get an entire object, overriding the specified response headers, and print the object's content.
+            ResponseHeaderOverrides headerOverrides = new ResponseHeaderOverrides()
+                    .withCacheControl("No-cache")
+                    .withContentDisposition("attachment; filename=example.txt");
+            GetObjectRequest getObjectRequestHeaderOverride = new GetObjectRequest(bucket_name, key)
+                    .withResponseHeaders(headerOverrides);
+            headerOverrideObject = amazonS3Client.getObject(getObjectRequestHeaderOverride);
+            displayTextInputStream(headerOverrideObject.getObjectContent());
+        }
+        catch(AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        }
+        catch(SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+        finally {
+            // To ensure that the network connection doesn't remain open, close any open input streams.
+            if(fullObject != null) {
+                fullObject.close();
+            }
+            if(objectPortion != null) {
+                objectPortion.close();
+            }
+            if(headerOverrideObject != null) {
+                headerOverrideObject.close();
+            }
         }
     }
 
+    private void displayTextInputStream(InputStream input) throws IOException {
+        // Read the text input stream one line at a time and display each line.
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        System.out.println();
+    }
 
-    public void initConnection() {
 
+
+    public void initRabbitMQConnection() {
 
         try {
 
@@ -83,30 +150,44 @@ public class EncoderCore {
         }
     }
 
+    public void createEncoderInstance() {
+
+
+        RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
+
+        runInstancesRequest.withImageId("ami-0ac019f4fcb7cb7e6") // subject to change
+                .withInstanceType(InstanceType.T2Micro) // subject to change
+                .withMinCount(1)
+                .withMaxCount(1)
+                .withUserData(Base64.getEncoder().encodeToString(new Scanner(ManagerInstance.class.getResourceAsStream("/encoder-instance.yml"), "UTF-8").useDelimiter("\\A").next().getBytes()))
+                .withKeyName("my-key-pair");
+
+        RunInstancesResult result = amazonEC2Client.runInstances(
+                runInstancesRequest);
+
+        System.out.println("Encoder instance launched with configuration: " + result);
+    }
+
 
     public static void main(String[] args) {
 
 
         EncoderCore core = new EncoderCore();
-        core.initConnection();
+        core.initRabbitMQConnection();
 
-//        for (S3ObjectSummary list : core.getFilesFromS3()) {
-//
-//            System.out.println(list);
-//        }
 
+        for (S3ObjectSummary list : core.listFilesS3()) {
+
+            System.out.println(list);
+        }
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+            core.getFileFromS3(message); //TODO: Tror inte jag har permission från KG's bucket att hämta filer.
+        };
         try {
-
-
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                Gson gson = new Gson();
-                Message message = gson.fromJson(new String(delivery.getBody(), "UTF-8"), Message.class);
-                System.out.println("BODY: " + new String(delivery.getBody()));
-                System.out.println(message.toString());
-            };
-
-            core.channel.basicConsume(ENCODING_REQUEST_QUEUE, true, deliverCallback, consumerTag -> {});
-
+            core.channel.basicConsume(ENCODING_REQUEST_QUEUE, true, deliverCallback, consumerTag -> { });
         } catch (IOException e) {
             e.printStackTrace();
         }
